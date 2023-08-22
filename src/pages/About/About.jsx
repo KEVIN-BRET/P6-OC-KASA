f75bcd86731f522cd7ed73423a1207611e07a1a9
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-08-22 11:25:50 +0200

    Mise en place du map des data des collapse "about"

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,19 +3,25 @@ import Banner from '../../components/Banner/Banner';
 import AboutBannerBg from '../../assets/images/about_banner_bg.webp'
 import Collapse from '../../components/Collapse/Collapse';
 import './About.scss';
+import aboutData from '../../assets/data/aboutData.json';
 
 const About = () => {
+
 	return (
 		<div>
 			<Banner title='À propos de nous' backgroundSrc={AboutBannerBg} />
-			<div className="collapse-container">
-
-				<Collapse title='Titre du Collapse' content='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' />
-				<Collapse title='Titre du Collapse' content='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' />
-				<Collapse title='Titre du Collapse' content='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' />
-				<Collapse title='Titre du Collapse' content='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' />
-
-			</div>
+			<ul className='collapse-container'>
+				{aboutData.map(({ index, title, content }) =>
+				(
+					<div key={`${index}-${title}`}>
+						<Collapse
+							title={title}
+							content={content}
+						/>
+					</div>
+				)
+				)}
+			</ul>
 		</div>
 	);
 };
