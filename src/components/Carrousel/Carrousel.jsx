4b6de5419c195f55e3424bee800feb25b9c111bb
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-08-23 16:47:23 +0200

    Carrousel bientot terminé (navigation ok)

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -2,17 +2,40 @@ import './Carrousel.scss';
 import { useState } from 'react';
 
 import logementObject from '../../assets/data/logementData.json';
-// import prevBtn from '../../assets/images/prev-btn.png'
-// import nextBtn from '../../assets/images/next-btn.png'
+import prevBtn from '../../assets/images/prev-btn.png'
+import nextBtn from '../../assets/images/next-btn.png'
 
 export function Carrousel({ slides }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-    return (
-        <div>
-            <img src={slides[currentIndex]} className='slides' alt={logementObject.title} />
-        </div>
-    );
-}
+	function goToNext() {
+		// 
+		if (currentIndex === slides.length - 1) {
+			setCurrentIndex(0);
+		} else {
+			setCurrentIndex(currentIndex + 1);
+		}
+	}
 
-export default Carrousel;
+	function goToPrevious() {
+		if (currentIndex === 0) {
+			setCurrentIndex(slides.length - 1);
+		} else {
+			setCurrentIndex(currentIndex - 1);
+		}
+	}
+
+		return (
+			<div>
+
+				<img className='nav-btn prev-btn' src={prevBtn} alt='précédente' onClick={goToPrevious}></img>
+
+				<img src={slides[currentIndex]} className='slides' alt={logementObject.title} />
+
+				<img className='nav-btn next-btn' src={nextBtn} alt='suivante' onClick={goToNext}></img>
+
+			</div>
+		);
+	}
+
+	export default Carrousel;
